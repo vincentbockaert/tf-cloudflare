7f167192a438c7f8a28fdd540822cef674f01ed8
Vincent Bockaert <bockaert.vincent1997@gmail.com>
2023-11-22 20:31:04 +0100

    Cleanup imports

--- accounts.tf
+++ accounts.tf
@@ -2,11 +2,3 @@ resource "cloudflare_account" "primary" {
   name              = var.cloudflare_account_name
   enforce_twofactor = true
 }
-
-
-import {
-  id = var.cloudflare_account_id
-  to = cloudflare_account.primary
-}
-
-# terraform import cloudflare_account.example <account_id>
--- zones.tf
+++ zones.tf
@@ -3,19 +3,3 @@ resource "cloudflare_zone" "this" {
   account_id = var.cloudflare_account_id
   zone       = each.key
 }
-
-import {
-  id = "da4ededf5601cd2297bf78305f0ee4cb"
-  to = cloudflare_zone.this["schaakclublievegem.be"]
-}
-
-
-import {
-  id = "93c9eef9c0b509ce821d1a3364042681"
-  to = cloudflare_zone.this["vincentbockaert.xyz"]
-}
-
-import {
-  id = "6e379f0f75bfb4587a70016ee2748742"
-  to = cloudflare_zone.this["techheresy.com"]
-}
