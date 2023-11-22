fbf357a1d0624031b66c73cc03e6f5290c086d15
Vincent Bockaert <bockaert.vincent1997@gmail.com>
2023-11-22 20:56:01 +0100

    Add explanation about how records are managed

--- zones.tf
+++ zones.tf
@@ -1,4 +1,5 @@
 resource "cloudflare_zone" "this" {
+  # note: records are managed through a different tool called OctoDNS, see https://github.com/vincentbockaert/dns
   for_each   = var.zones
   account_id = var.cloudflare_account_id
   zone       = each.key
